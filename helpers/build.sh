9d9efd7ee416bab55f29f4be8c52ce481b199790
Chris Landa <stylesuxx@gmail.com>
2016-08-03 03:14:47 +0200

    Remove include lines when building file for thingiverse

--- helpers/build.sh
+++ helpers/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cat workbench.scad helpers.scad 510.scad bottle.scad cutout.scad parametric_hammond.scad \
-	display.scad switch.scad sled.scad pcb.scad > box_mod.scad
+cat workbench.scad \
+	510.scad bottle.scad cutout.scad parametric_hammond.scad \
+	seed_technology_voltmeter.scad display.scad \
+	switch.scad sled.scad pcb.scad \
+	helpers.scad | sed -r 's/include <.*>;//g' > box_mod.scad
